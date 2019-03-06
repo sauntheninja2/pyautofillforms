c14258e1b1c548e51dc3393ccd562d01804f3c16
sauntheninja2 <48253973+sauntheninja2@users.noreply.github.com>
2019-03-06 06:17:43 +0530

    Update form.py

--- form.py
+++ form.py
@@ -4,7 +4,7 @@ import os
 webbrowser.open('') #the url of the form
 pyautogui.moveTo(968,100)
 for i in range(50,200): 
-                   pyautogui.rel(None,1) 
+                   pyautogui.moveRel(None,1) 
                    pyautogui.write("") #Whatever you want to fill
                    pyautogui.click(button="left")
 exit()
